bcc0b73246b7da40c5ebc1b5cbded4e829535324
Zeynep Özdemir <zeynepozz2299@gmail.com>
2023-04-04 15:34:09 +0300

    first commit

--- org/jd_bootcamp/project/HelloWorld.java
+++ org/jd_bootcamp/project/HelloWorld.java
@@ -3,6 +3,7 @@ package org.jd_bootcamp.project;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World! ");
+        // deneme ehfjglwj
     }
 
 }
